cbfb92760d1611cddc3ff8e5bd5c4c965fdfeb3d
Bethlehem Mesfin <mesfin.bethlehem@gmail.com>
2023-07-15 09:17:53 -0400

    Update streamlit_app.py
    Adding in alias names for abbrev

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,14 @@
-import streamlit
+import streamlit as st
 import pandas as pd
 
-streamlit.title('My Parent\'s New Healthy Diner')
-
-streamlit.header('Breakfast Menu')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
-
-streamlit.header('🍌🍓 Build Your Own Fruit Smoothie 🥝🍇')
+# Customizing menu text
+st.title('My Parent\'s New Healthy Diner')
+st.header('Breakfast Menu')
+st.text('🥣 Omega 3 & Blueberry Oatmeal')
+st.text('🥗 Kale, Spinach & Rocket Smoothie')
+st.text('🐔 Hard-Boiled Free-Range Egg')
+st.text('🥑🍞 Avocado Toast')
+st.header('🍌🍓 Build Your Own Fruit Smoothie 🥝🍇')
 
 # Read csv file in s3 bucket
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
