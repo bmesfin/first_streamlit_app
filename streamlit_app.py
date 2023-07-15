19a3fe5fbde4b52a97af2148bf38de693ba11846
Bethlehem Mesfin <mesfin.bethlehem@gmail.com>
2023-07-15 09:16:39 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-import pandas
+import pandas as pd
 
 streamlit.title('My Parent\'s New Healthy Diner')
 
@@ -12,5 +12,5 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🍓 Build Your Own Fruit Smoothie 🥝🍇')
 
 # Read csv file in s3 bucket
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list) # display the list
